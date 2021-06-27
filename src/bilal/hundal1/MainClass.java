b5c818b7dca142fedc449f5d738f79920ec08ff5
hs <hs@UMC-03456662441.Home>
2021-06-27 15:19:16 +0100

    First Commit

---
+++ src/bilal/hundal1/MainClass.java
@@ -0,0 +1,14 @@
+package bilal.hundal1;
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is main Class");
+		String Name="Bilal Hundal";
+		if(Name.startsWith("Bil")&& Name.endsWith("dal")) {
+			
+		}
+	}
+
+}
