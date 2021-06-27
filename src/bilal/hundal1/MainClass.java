1c6235c384deec7c683a994ab3ec4fabb2be0630
hs <hs@UMC-03456662441.Home>
2021-06-27 15:25:30 +0100

    updation in main

--- src/bilal/hundal1/MainClass.java
+++ src/bilal/hundal1/MainClass.java
@@ -7,7 +7,9 @@ public class MainClass {
 		System.out.println("This is main Class");
 		String Name="Bilal Hundal";
 		if(Name.startsWith("Bil")&& Name.endsWith("dal")) {
-			
+			System.out.println("This is Bilal Hundal");
+		}else {
+			System.out.println("This is Not Bilal");
 		}
 	}
 
